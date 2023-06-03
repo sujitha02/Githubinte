28b41b7452478b71e32262123f6bbf428d7777f9
sujitha02 <38128689+sujitha02@users.noreply.github.com>
2023-06-03 22:14:33 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,14 +4,3 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "access_key" {
-  description = "access key to connect aws"
-  type = string
-  default = "AKIA2D3AOYL4P3VMX6HV"
-}
-
-variable "secret_key" {
-  description = "secret key to connect aws"
-  type = string
-  default = "/qkeobc3O0H0JPfbxJGacKwr8mOXJqnB9FZpJju4"
-}
