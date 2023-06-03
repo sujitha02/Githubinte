71162a074739a816dc2fdc72ed72a0705a1e13c5
sujitha02 <38128689+sujitha02@users.noreply.github.com>
2023-06-03 21:52:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "aws region to cretate resource"
+  type = string
+  default = "us-east-1"
+}
+
+variable "access_key" {
+  description = "access key to connect aws"
+  type = string
+  default = "AKIA2D3AOYL4P3VMX6HV"
+}
+
+variable "secret_key" {
+  description = "secret key to connect aws"
+  type = string
+  default = "/qkeobc3O0H0JPfbxJGacKwr8mOXJqnB9FZpJju4"
+}
